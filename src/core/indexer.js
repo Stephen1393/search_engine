7514f488dea7b77c154c92c418c01ffd5d22458d
Stephen1393 <stephen.bending20@gmail.com>
2026-02-17 15:40:28 +0000

    remove comments from indexer.

--- src/core/indexer.js
+++ src/core/indexer.js
@@ -1,36 +1,36 @@
 const { tokenize } = require('./tokenize') 
-const fs = require('fs') //require fs for local files
-const path = require('path') //require path to know where to join from -- to
+const fs = require('fs') 
+const path = require('path') 
 
 const indexBuilder = () => {
     const docIdToName = {}
     const index = {}
 
-    const list = fs.readdirSync(DOCS) //list files
+    const list = fs.readdirSync(DOCS) 
 
-    const files = list.filter(f => f.endsWith(".txt")) //filter files ending with .txt
+    const files = list.filter(f => f.endsWith(".txt")) 
          
-    files.sort() //sort files ending it .txt
+    files.sort() 
 
      const DOCS = path.join(__dirname,'..','..','docs') //joining to the correct folder. Used to build docs.
 
     for (let i = 0; i < files.length; i++) {
-        const docId = i //is the position of i (0,1,2,3,etc)
-        const filename = files[i] //name of the file at the index
-        docIdToName[docId] = filename //this is the number (i) to equal filename. "0 computerscience.txt"
+        const docId = i 
+        const filename = files[i] 
+        docIdToName[docId] = filename 
          
         const fullpath = path.join(DOCS, filename) //path to filename from DOCS
         const text = fs.readFileSync(fullpath, "utf8")//"utf8". Nodes decodes raw bytes in english
 
-        const tokens = tokenize(text) //the result of the tonkenize script
+        const tokens = tokenize(text) 
 
-        const uniqueTokens = new Set(tokens) //get rid of duplicates -- repeated words
+        const uniqueTokens = new Set(tokens) 
         
-        for (const token of uniqueTokens) { //for..of is interested in values, not indexes
-            if (!index[token]) {  //Seen the token before?
-                index[token] = []//...add to postings
+        for (const token of uniqueTokens) { 
+            if (!index[token]) {  
+                index[token] = []
             }
-            index[token].push(docId) //push the token with the doc(s) Id   
+            index[token].push(docId)   
         }
     }
 
