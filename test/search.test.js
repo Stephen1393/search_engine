6e4c4981db2ddef76941adb294e9fa5faef9ff9e
Stephen1393 <stephen.bending20@gmail.com>
2026-03-05 16:05:48 +0000

    add more test docs and complete search tests.

--- test/search.test.js
+++ test/search.test.js
@@ -1,4 +1,3 @@
-const { indexBuilder } = require('../src/core/indexer')
 const { createSearch } = require('../src/core/search')
 const path = require('path')
 
@@ -16,9 +15,33 @@ describe('search...', () => {
 
     it('maps docId and filename to correct query', () => {
 
-        const results = search("hello world!!!")
+        const results = search("hello everyone")
 
-        expect(results).toEqual([{ id: 3, filename: "doc04.txt" }])
+        expect(results.length).toBeGreaterThan(0)
+        expect(results).toContainEqual({ id: 0, filename: "doc01.txt" })
 
+    })
+
+    it('returns empty array with single token miss...', () => {
+        
+        const result = search("pencil")
+
+        expect(result).toEqual([])
+    })
+
+    it('returns empty array if doc doesnt include both tokens. (Use of AND)', () => {
+
+        const results = search('Hello there')
+
+        expect(results).toEqual([])
+    })
+
+    it('returns a limit of 5 matching docs...', () => {
+
+         const results = search("good morning")
+         const ids = results.map(r => r.id)
+
+         expect(results).toHaveLength(5)
+         expect(ids).toEqual([5,6,7,8,9])
     })
 })
