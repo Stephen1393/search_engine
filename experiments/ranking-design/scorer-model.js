ee9270b5c4d86eec88cfa5553b466712bc884065
Stephen1393 <stephen.bending20@gmail.com>
2026-04-08 11:25:29 +0100

    feat: add proximity scoring using tokenPositions

--- experiments/ranking-design/scorer-model.js
+++ experiments/ranking-design/scorer-model.js
@@ -1,8 +1,9 @@
 const { tokenize } = require('../../src/core/tokenize')
 
+let score = 0
+
 const scorer = (indexer, query, docId) => {
 
-    let score = 0
     const queryTokens = tokenize(query)
     const titleTokens = indexer.docMeta[docId].titleTokens
 
@@ -34,6 +35,56 @@ const scorer = (indexer, query, docId) => {
 
         }
     }
-}
-return scorer
 
+    const proximity = (indexer, query, docId) => {
+
+       const queryTokens = tokenize(query)
+       const tokenPositions = indexer.docMeta[docId].titlePositions
+       let result = 0
+       let partial = false
+       let current
+
+       for (let i = 0; i < queryTokens.length - 1; i++) {
+        let tokenA = queryTokens[i]
+        let tokenB = queryTokens[i + 1]
+    
+        if (tokenPositions[tokenB] === undefined) {
+            return result
+        }
+
+        if (i === 0) {
+            current = tokenPositions[tokenA]
+        }
+
+        let matches = []
+
+        for (let j = 0; j < current.length; j++) {
+            let tokenLocation = current[j]
+        
+
+        for (let x = 0; x < tokenPositions[tokenB].length; x++) {
+            let tokenLocation2 = tokenPositions[tokenB][x]
+
+            if (tokenLocation2 === tokenLocation + 1) {
+                matches.push(tokenLocation2)
+            }
+        }
+
+    }
+
+        current = matches
+
+        if (matches.length === 0) { return result}
+
+        if (matches.length > 0 && partial === false) {
+            partial = true
+            result += 1
+        }
+        if (i === queryTokens.length -2 && matches.length > 0) {result += 1}
+
+
+    }
+    return result
+}
+
+}
